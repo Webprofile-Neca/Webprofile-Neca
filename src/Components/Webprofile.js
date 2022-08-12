4ed12282b977e1ca339dcc65dec1bd9b9ec95fd6
unknown <muelinx@gmail.com>
2022-08-12 15:41:01 +0100

    first commit for component1

---
+++ src/Components/Webprofile.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import'./Webprofile.css'
+import arrowicon from './arrowicon.png'
+import nextt from './nextt.png'
+import previous11 from './previous11.png'
+
+
+function Webprofile() {
+  return (
+    <div className='main'>
+      <div className='firstdiv'>
+          <div className='updiv'>
+             <div className='title'>Webprofile</div>
+             <nav><li>Home</li><li>Mission</li><li>About</li><li>Contact</li></nav>
+         
+         </div>
+     
+             
+     </div>
+            <div className='seconddiv'>
+
+                <div className='up1'>
+                     <div className='a'><h1>Discover Innovative ways to decorate</h1></div>
+                     <div className='b'><p>We provide jveufhufhr hjibrufrfr hjbyurffuihr ibygfyf byufef byuefge byuigeuf
+                         iuhifre iuhufhf yu8huihfuf u8huhuehf og5ijgi 9uhvrh u9h8yvr8 uhuhgrg
+                        </p></div>
+                     <div className='c'>Click here for more<img className='more' src={arrowicon} alt='arrowicon'/></div>
+                </div>
+
+
+                <div className='down2'><button><img className='arrow' src={previous11} alt='previous11'/></button><button><img className='arrow' src={nextt} alt='nextt'/></button></div>
+       
+           </div>
+
+    </div>
+  )
+}
+
+export default Webprofile
