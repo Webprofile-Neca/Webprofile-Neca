a5e1db6cac958152632af6948b44468933dd30a9
unknown <rashysmooth@yahoo.com>
2022-08-12 16:08:12 +0100

    component2 ready

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import Webprofile from './Components/Webprofile';
+import ComponentTwo from './Components/ComponentTwo';
 
 
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
     <Webprofile/>
+    <ComponentTwo/>
 
     </div>
   );
