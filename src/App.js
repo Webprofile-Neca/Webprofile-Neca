e022e8a1d3b03163252645c4af30d6c36e1907bb
unknown <rashysmooth@yahoo.com>
2022-08-15 12:29:30 +0100

    New UI done!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import InputSearch from './InputSearch';
+import WebProfiles from './WebProfiles';
+
+import users from './items.json';
 
 function App() {
+
+const [currentUserId, setCurrentUserId] = useState(1);
+const [currentInput, setCurrentInput] = useState("");
+const [suggestions, setSuggestions] = useState([]);
+
+const filterSuggestions = (searchUpdate) => {
+  setCurrentInput(searchUpdate);
+  if (searchUpdate){
+    setSuggestions(
+      users.filter((user) => user.name.toLowerCase().includes(searchUpdate.toLowerCase()))
+    );
+  }else{
+    setSuggestions([]);
+  }
+  
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <InputSearch setCurrentUserId={setCurrentUserId } currentInput={currentInput} setCurrentInput={setCurrentInput} suggestions={suggestions} setSuggestions={setSuggestions} filterSuggestions={filterSuggestions} />
+      <WebProfiles users={users} currentUserId={currentUserId} setCurrentUserId={setCurrentUserId}/>
     </div>
-  );
+  )
 }
 
 export default App;
