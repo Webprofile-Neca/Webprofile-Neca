ed3b2bf03457a7c26dde7e89645a393938167c6e
Harrison <harrisonderick.65@gmail.com>
2022-08-16 20:10:08 +0100

    input updated on new UI

--- src/InputSearch.jsx
+++ src/InputSearch.jsx
@@ -3,15 +3,19 @@ import './InputSearch.css';
 
 function InputSearch({setCurrentUserId, currentInput, setCurrentInput, suggestions, setSuggestions, filterSuggestions}) {
   return (
-    <div className='input-search'>
-        <h1>WEBPROFILES</h1>
-        <input type="text" className='input' placeholder='Search Profile Name' value={currentInput} onChange={(event) => { filterSuggestions(event.target.value) }} />
-        <button>Search</button>
-        <div className='suggestions'>
-           {suggestions.map((user) => (
-            <p onClick={() => {setCurrentUserId(user.id)}}>{user.name}</p>
-           ))}
+    <div className="search-box">
+      <h1>WEBPROFILES</h1>
+      <div className='input-search'>
+          <input type="text" className="search" placeholder="Search Profile Name" value={currentInput} onChange={(event) => { filterSuggestions(event.target.value) }} />
+          {/* <button>Search</button> */}
+
+        <div className='suggestion-box'>
+          {suggestions.map((user, id) => (
+            <p key={id} className="suggestions" onClick={() => {setCurrentUserId(user.id)}}>{user.name}</p>
+          ))}
         </div>
+        
+      </div>
     </div>
   )
 }
--- src/WebProfiles.jsx
+++ src/WebProfiles.jsx
@@ -40,17 +40,20 @@ const prev = () => {
     <div className='main'> 
         <button  onClick={() => {prev()}} className='left-angle'>{'<'}</button>
         <div className='container'>
+
         <div className='about'>
             <div className='about-me'>
-            <h1>Hi! My Name is <span>{currentUser.name}</span>.</h1>
-            <p>I'm a <span>Software developer</span>. {currentUser.about}</p>
-            <p>Skills: <span>{currentUser.skills}</span> </p>
-            <p>Connect with me: <span><a href={currentUser.github_link} target="blank">{currentUser.github_link}</a></span></p>
+                <h1>Hi! My Name is <span>{currentUser.name}</span>.</h1>
+                <p>I'm a <span>Software developer</span>. {currentUser.about}</p>
+                <p>Skills: <span>{currentUser.skills}</span> </p>
+                <p>Connect with me: <span><a href={currentUser.github_link} target="blank">{currentUser.github_link}</a></span></p>
             </div>  
         </div>
+
         <div className='image'>
         <img src = {require(`./Pictures/${currentUser.img}`)} alt = 'profile'></img>
         </div>
+
     </div>
     <button onClick={() => {next()}} className='right-angle'>{'>'}</button>
     </div>
